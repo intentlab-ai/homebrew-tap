391fb0caaff83f46d3d3e32a3da8e3d1aa32fffb
jfan520 <hhyuan.f@gmail.com>
2026-05-12 00:29:38 -0700

    tentrix-cli v0.1.5 (prod) (#9)
    Co-authored-by: Jingyuan Fan <jy@intentlab.ai>

--- Formula/tentrix-cli.rb
+++ Formula/tentrix-cli.rb
@@ -1,26 +1,26 @@
 class TentrixCli < Formula
   desc "Coding agents on Tentrix"
   homepage "https://github.com/intentlab-ai/tentrix"
-  version "0.1.4"
+  version "0.1.5"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://cli-dev.intentlab.ai/tentrix-cli/releases/v0.1.4/tentrix-cli_v0.1.4_darwin_arm64.tar.gz"
-      sha256 "e13511bf527080c25338e1b8c4dce1c6b429b4d8a63f26f9b35066b864feebc9"
+      url "https://cli.intentlab.ai/tentrix-cli/releases/v0.1.5/tentrix-cli_v0.1.5_darwin_arm64.tar.gz"
+      sha256 "c3e5573635f77fd3e6395613a4a7274ddc1396c08bf18b63b64c5cc034f4a571"
     else
-      url "https://cli-dev.intentlab.ai/tentrix-cli/releases/v0.1.4/tentrix-cli_v0.1.4_darwin_amd64.tar.gz"
-      sha256 "04a932c816830d092422cec0326537f25911a2606c8371cb309a9d36e51036da"
+      url "https://cli.intentlab.ai/tentrix-cli/releases/v0.1.5/tentrix-cli_v0.1.5_darwin_amd64.tar.gz"
+      sha256 "468e6dd755923940d8bc95caf804e6cfa77f8e7cc05433d590956dcdaf6d7b5a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://cli-dev.intentlab.ai/tentrix-cli/releases/v0.1.4/tentrix-cli_v0.1.4_linux_arm64.tar.gz"
-      sha256 "283600e4dbcb82f85b0282deae03c8b6d0ff4b368af4b30c9aa46f27f36e87ef"
+      url "https://cli.intentlab.ai/tentrix-cli/releases/v0.1.5/tentrix-cli_v0.1.5_linux_arm64.tar.gz"
+      sha256 "068b69ee9579421842734fa73709e515ff0a18d051c9ef515a0c6fe05a3bc980"
     else
-      url "https://cli-dev.intentlab.ai/tentrix-cli/releases/v0.1.4/tentrix-cli_v0.1.4_linux_amd64.tar.gz"
-      sha256 "7ff7c7bdd40c3f7503582df126bd9bf8c830b7852226aa5c14d3228b63f50150"
+      url "https://cli.intentlab.ai/tentrix-cli/releases/v0.1.5/tentrix-cli_v0.1.5_linux_amd64.tar.gz"
+      sha256 "cf0fc3ccf2865df52e85e2929b93b8144091807680eefb54810af8786803e788"
     end
   end
 
